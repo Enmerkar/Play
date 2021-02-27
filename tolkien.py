db5d9501461b5f919bc7e61e0ece10145da5ca11
Justin Knife <jkni3047@uni.sydney.edu.au>
2021-02-28 10:31:48 +1100

    updates

--- tolkien.py
+++ tolkien.py
@@ -12,10 +12,10 @@ Could even try blends: e.g. Tolkien + Dan Brown.
 """
 
 # Read Ubuntu
-hobbit = open('/home/justin/Documents/Github/Play/data/hobbit.txt')
-fellowship_ring = open('/home/justin/Documents/Github/Play/data/fellowship_ring.txt')
-two_towers = open('/home/justin/Documents/Github/Play/data/two_towers.txt')
-return_king = open('/home/justin/Documents/Github/Play/data/return_king.txt')
+hobbit = open('/home/justin/Downloads/hobbit.txt')
+fellowship_ring = open('/home/justin/Downloads/fellowship_ring.txt')
+two_towers = open('/home/justin/Downloads/two_towers.txt')
+return_king = open('/home/justin/Downloads/return_king.txt')
 
 # Read Mac
 hobbit = open('/Users/justinknife@qantas.com.au/Documents/hobbit.txt')
@@ -24,7 +24,10 @@ two_towers = open('/Users/justinknife@qantas.com.au/Documents/two_towers.txt')
 return_king = open('/Users/justinknife@qantas.com.au/Documents/return_king.txt')
 
 # Clean the hobbit
-hobbit_lines = hobbit.readlines()
+# Or
+with open('/home/justin/Downloads/hobbit.txt') as hobbit:
+    hobbit_lines = hobbit.readlines()
+
 hobbit_lines_filtered = list(filter(lambda x: x!='\n', hobbit_lines))
 hobbit_lines_filtered = hobbit_lines_filtered[59:1937]
 chapter_index = [i for i, x in enumerate(hobbit_lines_filtered) if x.startswith('Chapter ')]
@@ -32,25 +35,14 @@ chapter_head = chapter_index + [i+1 for i in chapter_index]
 keep_lines = set([*range(len(hobbit_lines_filtered))])
 keep_lines -= set(chapter_head)
 hobbit_clean = [line for i, line in enumerate(hobbit_lines_filtered) if i in keep_lines]
-
-# Close all books
-hobbit.close()
-fellowship_ring.close()
-two_towers.close()
-return_king.close()
-
-
-# Or
-with open('/home/justin/Documents/Github/Play/data/hobbit.txt') as hobbit:
-    hobbit_lines = hobbit.readlines()
-
-# Remove blank lines.
-# Delete top matter.
-# Delete "Chapter" and succeeding rows.
+hobbit_string = ' '.join(hobbit_clean)
 
 import spacy
-
 nlp_en = spacy.load('en_core_web_sm')
+
+#hobbit_utf = hobbit_string.encode(encoding='UTF-8', errors='strict')
+
+hobbit_doc = nlp_en(hobbit_string)
 
 doc_en = nlp_en(u'My name is Justin and I am a human who likes eating various foodstuffs.')
 
@@ -59,6 +51,17 @@ for token in doc_en:
     
 nlp_en.pipeline
 
+hobbit_doc
+
+# TF-IDF
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+vect = TfidfVectorizer()
+dtm = vect.fit_transform(hobbit_clean)
+print(vect.get_feature_names())
+
+c_vect = CountVectorizer()
 
 
 
