4598fba9a216d27ca34850c72b565cd80dd4541a
Justin Knife <jkni3047@uni.sydney.edu.au>
2021-03-01 06:27:17 +1100

    Cleaned hobvbit data

--- tolkien.py
+++ tolkien.py
@@ -55,13 +55,51 @@ hobbit_doc
 
 # TF-IDF
 
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split
 
 vect = TfidfVectorizer()
 dtm = vect.fit_transform(hobbit_clean)
 print(vect.get_feature_names())
 
-c_vect = CountVectorizer()
+hobbit_df = pd.DataFrame(hobbit_lines_filtered)
+hobbit_df.columns = ['Text']
+chapter_df = pd.DataFrame(chapter_index)
+chapter_df.columns = ['Line']
+chapter_df['Chapter'] = chapter_df.index + 1
+hobbit_df['Chapter'] = 0
+for i, row in chapter_df.iterrows():
+    hobbit_df['Chapter'][row[0]] = row[1]
+chapter = 0
+for i, row in hobbit_df.iterrows():
+    if row[1] > 0: chapter = row[1]
+    hobbit_df['Chapter'][i] = chapter
+
+X_train, X_test, y_train, y_test = train_test_split(hobbit_df['Text'],
+                                                    hobbit_df['Chapter'],
+                                                    test_size=0.3,
+                                                    random_state=42)
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+
+X_train.shape
+X_train_counts.shape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
